164700aed84d0ec8a2308b86b9c323379491b138
سالم التريمي <254166838+saltrymy97-eng@users.noreply.github.com>
2026-06-22 04:46:14 +0300

    Add main.js for Electron application setup

---
+++ electron/main.js
@@ -0,0 +1,36 @@
+// electron/main.js – بديل احتياطي لبناء .exe
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 900,
+    minHeight: 600,
+    icon: path.join(__dirname, '../public/icon.ico'),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, '../build/index.html'));
+  mainWindow.setMenuBarVisibility(false);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
